a405f0407118d1128ab9844d0de003a2dfbede57
Arslan952 <108229469+Arslan952@users.noreply.github.com>
2023-06-23 15:26:54 +0500

    Fist

---
+++ lib/growinganimaton.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GrowingAnimation extends StatefulWidget {
+  const GrowingAnimation({super.key});
+
+  @override
+  State<GrowingAnimation> createState() => _GrowingAnimationState();
+}
+
+class _GrowingAnimationState extends State<GrowingAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
